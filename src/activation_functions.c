41e296675dcf14b43ee514a29a0330d8758bca11
xfeja <xfeja@fi.muni.cz>
2023-10-10 09:24:26 +0200

    activation functions

---
+++ src/activation_functions.c
@@ -0,0 +1,11 @@
+#include "activation_functions.h"
+
+
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
+double sigmoid_derivative(double x) {
+    double s = sigmoid(x);
+    return s * (1.0 - s);
+}
---
+++ src/activation_functions.h
@@ -0,0 +1,6 @@
+#include <math.h>
+
+
+double sigmoid(double x);
+
+double sigmoid_derivative(double x);
