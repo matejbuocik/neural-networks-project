8c54d5b21077ad519ffd35099f52dae99a4dc33e
xfeja <xfeja@fi.muni.cz>
2023-10-11 21:11:16 +0200

    it look like it works :)

--- src/model_xor.c
+++ src/model_xor.c
@@ -19,16 +19,16 @@ int main() {
     //print_matrices(inputs_array, in_n);
     //print_matrices(outputs_array, in_n);
 
-    int hidden_layer_sizes[1] = {4};
-    func_ptr activation_funs[2] = {&ReLU, &sigmoid};
-    func_ptr activation_funs_der[2] = {&ReLU_der, &sigmoid_der};
+    int hidden_layer_sizes[1] = {8};
+    func_ptr activation_funs[2] = {&sigmoid, &sigmoid};
+    func_ptr activation_funs_der[2] = {&sigmoid_der, &sigmoid_der};
 
     MLP mlp = create_mlp(inputs_array[0]->cols, outputs_array[0]->cols, 1, hidden_layer_sizes,
                          activation_funs, activation_funs_der);
 
-    initialize_weights(&mlp, 42, -0.001, 0.001);
+    initialize_weights(&mlp, 42, -10, 10);
 
-    train(&mlp, in_n, inputs_array, outputs_array, 0.1, 10000000, 10);
+    train(&mlp, in_n, inputs_array, outputs_array, 0.001, 10000000, 1);
 
     double test_res = test(&mlp, in_n, inputs_array, outputs_array, NULL);
 
